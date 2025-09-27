745ba12c9772e892c46dd0704f00efaae20d3106
bururun <bururun@users.noreply.github.com>
2025-09-27 16:42:52 +0300

    build(ui): fix null pointer exception

---
+++ lib/helpers.py
@@ -0,0 +1,9 @@
+# Helper functions
+
+def helper_function_7(x):
+    """Helper function for iteration 7."""
+    return x * 7
+
+def format_output(data):
+    """Format output data."""
+    return str(data).upper()
